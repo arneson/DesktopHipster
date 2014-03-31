534bdac6ed115a0e2378723a28a2c2dc55439cf7
Edvard Hübinette <e.hubinette@gmail.com>
2014-03-31 16:54:10 +0200

    Added converter method from Image -> buffered image

--- DesktopHipster/src/main/java/Filter/ImageTools.java
+++ DesktopHipster/src/main/java/Filter/ImageTools.java
@@ -2,6 +2,8 @@ package Filter;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 public class ImageTools {
@@ -89,5 +91,25 @@ public class ImageTools {
 	    g.drawImage(source, 0, 0, null);
 	    g.dispose();
 	    return b;
+	}
+	
+	//From stackOverflow
+	public static BufferedImage toBufferedImage(Image img)
+	{
+	    if (img instanceof BufferedImage)
+	    {
+	        return (BufferedImage) img;
+	    }
+
+	    // Create a buffered image with transparency
+	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+	    // Draw the image on to the buffered image
+	    Graphics2D bGr = bimage.createGraphics();
+	    bGr.drawImage(img, 0, 0, null);
+	    bGr.dispose();
+
+	    // Return the buffered image
+	    return bimage;
 	}
 }
