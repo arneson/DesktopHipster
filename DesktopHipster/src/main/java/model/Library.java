8eccdd31acbdc40969983f5fc1fbe85f65b684b0
Lovisa Jäberg <jaberglovisa@gmail.com>
2014-04-06 15:06:35 +0200

    Created AddPanel

--- DesktopHipster/src/main/java/model/Library.java
+++ DesktopHipster/src/main/java/model/Library.java
@@ -7,24 +7,53 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
+/*
+ * @authour Lovisa Jaberg
+ * The Library will keep track of the imported images.
+ */
+
+//TODO Must save the list with images to disc in hidden directory when you quit the application
+//TODO 
+
 public class Library {
 
 	//Array of extended images
 	private ArrayList<BufferedImage> imageArray = new ArrayList();
 	
 	//Path to the directory where you save the images
-	private Path path = Paths.get(System.getProperty("user.home") + "/Pictures/LovisPro");
+	private Path path;
 	
 
 	public Library(){
 		
-		// Create a directory; all non-existent ancestor directories are
-		// automatically created
+		/*
+		 * Create a directory where you save your images. 
+		 * If this directory is already created then it sets the directory where to save to the created directory.
+		 */
+		
 		boolean success = (new File(System.getProperty("user.home") + "/Pictures/DesktopHipster")).mkdirs();
 		
 		if (!success) {
+			path = Paths.get(System.getProperty("user.home") + "/Pictures/DesktopHipster");
 			System.out.println("Directory already excists");
-			
 		}
+	}
+	
+	/*
+	 * Saves image to disc in the application folder
+	 */
+	
+	public void save(BufferedImage saveImage){
+		
+	}
+	
+	/*
+	 * When you import an image to the application by choosing from 
+	 * file system or drops it in drop down panel this method will 
+	 * put it into the arraylist of images it will show in the library
+	 */
+	
+	public void load(){
+		
 	}
 }
---
+++ DesktopHipster/src/main/java/view/AddPanel.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class AddPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public AddPanel() {
+		setBackground(Color.WHITE);
+		
+	}
+
+}
