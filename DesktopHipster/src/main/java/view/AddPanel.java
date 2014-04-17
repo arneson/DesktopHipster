c12c8cd2601f77aca48bc30cd7f6835b7baf357c
Lovisa Jäberg <jaberglovisa@gmail.com>
2014-04-17 09:54:47 +0200

    Added functionality to AddPanel

--- DesktopHipster/src/main/java/view/AddPanel.java
+++ DesktopHipster/src/main/java/view/AddPanel.java
@@ -1,11 +1,18 @@
 package view;
 
+import general.PropertyNames;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
 
 /**
  * 
@@ -14,9 +21,10 @@ import java.awt.event.MouseEvent;
  */
 
 public class AddPanel extends JPanel {
-	
+
 	private JLabel label;
-	
+
+
 
 	/**
 	 * Create the panel.
@@ -25,9 +33,23 @@ public class AddPanel extends JPanel {
 		setLayout(new BorderLayout());
 		setBackground(Color.WHITE);
 		setToolTipText("Add image to library");
+		setCursor(new Cursor(Cursor.HAND_CURSOR));
 		label = new JLabel(new ImageIcon(getClass().getResource("/AddPanel.png")));
+		label.addPropertyChangeListener(new PropertyChangeListener() {
+			public void propertyChange(PropertyChangeEvent evt) {
+				JFileChooser chooseFile = new JFileChooser();
+				FileNameExtensionFilter allowedSuffix = new FileNameExtensionFilter("JPG, PNG, & GIF", ".jpg", ".png", ".gif");
+				chooseFile.setFileFilter(allowedSuffix);
+				int file = chooseFile.showOpenDialog(getParent());
+				if(file == JFileChooser.APPROVE_OPTION) {
+	    	    	firePropertyChange(PropertyNames.ADD_NEW_IMAGE_TO_LIBRARY,null,chooseFile.getSelectedFiles());
+				}
+			}
+		});
 		label.setOpaque(true);
 		add(label,BorderLayout.CENTER);
 	}
+
+
 
 }
