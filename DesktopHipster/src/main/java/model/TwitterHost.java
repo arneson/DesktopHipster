22828cc3e48c224277cae6903d34b47319484321
sveningsonrobin <robinsveningson@gmail.com>
2014-05-23 14:05:34 +0200

    Removed white thing in versions panel. Removed warnings.

--- DesktopHipster/src/main/java/model/TwitterHost.java
+++ DesktopHipster/src/main/java/model/TwitterHost.java
@@ -2,7 +2,6 @@ package model;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
